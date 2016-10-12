8406ddb1504208663e337953e6f7f564becaff54
digiguru <digiguru@gmail.com>
2016-10-12 22:21:46 +0100

    Authentication now works

--- auth/config/auth.js
+++ auth/config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    /*'facebookAuth' : {
+    'facebookAuth' : {
         'clientID'      : 'your-secret-clientID-here', // your App ID
         'clientSecret'  : 'your-client-secret-here', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
@@ -10,7 +10,7 @@ module.exports = {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
         'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
-    },*/
+    },
 
     'googleAuth' : {
         'clientID'      : '620528950538-4r0mph5q5s5b72civffpdlgsvmnm42je.apps.googleusercontent.com',
