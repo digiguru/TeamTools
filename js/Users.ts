e86fdbb037e921dbdb56f5852af2d4d43d9669f2
Adam Hall <adam.hall@hitched.email>
2016-11-28 17:25:55 +0000

    prep file

--- js/Users.ts
+++ js/Users.ts
@@ -1,8 +1,24 @@
 import {User} from '../Shared/User';
 
-export class User {
-    getUsers : Array<User> {
-        
-        return [];
+export class Users {
+    users : Array<User>;
+    constructor() {
+        this.users = [
+            new User("Adam Hall","xxx1"), 
+            new User("Billie Davey","xxx2"), 
+            new User("Laura Rowe","xxx3"),
+            new User("Simon Dawson","xxx4")
+        ]
+    }
+    getUsers() : Array<User>  {
+        return this.users;
+    }
+    setUsers(names:Array<string>) {
+        this.users = [];
+        for (var i=0; i<names.length;i++) {
+            this.users.pop(new User(names[i], "user"+i))
+        }
+            
+        });
     }
 }
