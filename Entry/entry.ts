18788cd896f5aa0b4076c3c507ef891ecb73e36d
Adam Hall <adam.hall@hitched.email>
2017-01-12 17:45:40 +0000

    Allow entry of users one at a time

--- Entry/entry.ts
+++ Entry/entry.ts
@@ -3,35 +3,79 @@
 /// <reference path="../typings/requirejs/require.d.ts"/>
 /// <reference path="../Shared/InMemoryBrowserUsers.ts"/>
 /// <reference path="../Shared/UserConstructor.ts"/>
-var users;
 requirejs.config( {
     baseUrl : '/'
 });
 
+var users;
 
 require(['Shared/InMemoryBrowserUsers', 'Shared/UserConstructor'], function(u, c) {
 
     console.log("Starting");
     users = new u.InMemoryBrowserUsers(window);
     
+    var getUsers = () => {
+        var entry = document.getElementById('users').getElementsByClassName('user');
+        var usernames = new Array<string>();
+        for (var i=0;i<entry.length;i++) {
+            usernames.push(entry.item(i).textContent);
+        }
+        return usernames;            
+    };
+    var saveUsers = (usernames) => {
+        var newusers = c.UserConstructor.createUsersByNames(usernames);
+        window.users.setUsers(newusers).then((result) => {
+            console.log("Set users", result);
+        });
+    }
+    var UIAddUser = (username) => {
+        var newNode = document.createElement("li");
+        newNode.setAttribute("class", "user");
+        newNode.textContent = username;
+        document.getElementById('users').appendChild(newNode);
+    }
+    var UIClearUser = (username) => {
+        var parent = document.getElementById('users');
+        var nodeList = parent.childNodes;
+        for (var i=nodeList.length;i--;i>0) {
+            if(username === nodeList[i].textContent) {
+                document.getElementById('users').removeChild(nodeList[i]);
+            }  
+        }
+    }
+    var UIClearUsers = () => {
+        var parent = document.getElementById('users');
+        var nodeList = parent.childNodes;
+        for (var i=nodeList.length;i--;i>0){
+            document.getElementById('users').removeChild(nodeList[i]);
+        }
+    }
     users.getUsers().then((data) => {
+        UIClearUsers();
         console.log("Have these users", data);
         if (data && data.length) {
             const strings:string[] = data.map((user) => {
                 return user.name;
             });
             console.log("see", strings);
-            const text = strings.join("\n");
+            for (var i=0;i<strings.length;i++) {
+                console.log("ADD", strings[i]);
+                UIAddUser(strings[i]);
+            }
+            /*const text = strings.join("\n");
             console.log("is", text);
-            document.getElementById('users').value = text;
+
+            document.getElementById('user').value = text;
+            */
         }
+
         document.getElementById('save').addEventListener("mousedown", () => {
-            var entry :string = document.getElementById('users').value;
-            var usernames = entry.split("\n");
-            var newusers = c.UserConstructor.createUsersByNames(usernames);
-            window.users.setUsers(newusers).then((result) => {
-                console.log("Set users", result);
-            });
+            var username = document.getElementById('user').value;
+            if (username) {
+                UIAddUser(username);
+                var usernames = getUsers();
+                saveUsers(usernames);
+            }
         });
     });
     
