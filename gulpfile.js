69108296b295320c9b0f6fde15e1aa4644186f2a
Adam Hall <adam.hall@hitched.email>
2017-01-14 23:05:40 +0000

    Fixed compilation to use proper source maps

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,25 @@ var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task("Entry", function () {
-    return tsEntry.src()
+    var ts = tsEntry.src()
         .pipe(sourcemaps.init())
         .pipe(tsEntry())
-        .js.pipe(sourcemaps.write())
+    return ts.js
+        .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("Entry"));
 });
 gulp.task("Tuckman", function () {
     return tsTuckman.src()
         .pipe(sourcemaps.init())
         .pipe(tsTuckman())
-        .js.pipe(sourcemaps.write())
+        .js.pipe(sourcemaps.write("."))
         .pipe(gulp.dest("Tuckman"));
 });
 gulp.task("ComfortZone", function () {
     return tsComfortZone.src()
         .pipe(sourcemaps.init())
         .pipe(tsComfortZone())
-        .js.pipe(sourcemaps.write())
+        .js.pipe(sourcemaps.write("."))
         .pipe(gulp.dest("ComfortZone"));
 });
 
