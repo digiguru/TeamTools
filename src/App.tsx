30885f9202fc55c4333efc21b65d20cfd2731a3a
digiguru <digiguru@gmail.com>
2020-04-29 00:58:18 +0100

    Entry added to form

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import ComfortStore from "./React/Comfort/Store"
 import TuckmanStore from "./React/Tuckman/Store"
 import './Shared/styles.css'
+import Entry from "./Entry/Entry"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-       
+        <h1>Team Tools</h1>
       </header>
+      <h3>Entry</h3>
+      <Entry />
+      <h2>Comfort Model</h2>
       <ComfortStore />
+      <h2>Tuckman Model</h2>
       <TuckmanStore />
+      
     </div>
   );
 }
---
+++ src/Entry/Entry.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import UIEntry from './UIEntry';
+function Entry() {
+    return (
+        <div>
+            <p>The entry form is currently broken, but I'm sure it will be straightforward to get it working, just convert 128 lines of vanilla javascript mangle into React, right?</p>
+            <input type="button" value="new team" id="new" />
+
+            <ul id="users">
+            </ul>
+
+            <input type="text" placeholder="Joe Bloggs" id="user" />
+            <input type="button" value="add" id="add" />
+            <ul>
+            <li><a href="/react/react.html?model=ComfortZone">Comfort Zone</a></li>
+            <li><a href="/react/react.html?model=Tuckman">Tuckman</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Entry;
