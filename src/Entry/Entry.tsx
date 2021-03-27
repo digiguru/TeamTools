60e0f7631f025470144474a1fd1abd56f8c38e91
digiguru <digiguru@gmail.com>
2021-03-27 09:27:47 +0000

    Clean up - remove old user entry

--- src/Entry/Entry.tsx
+++ src/Entry/Entry.tsx
@@ -13,16 +13,8 @@ function Entry() {
     }
     return (
         <div>
-            <p>react version</p>
             <UserEntry message="React" handleUserListChange={handleUserListChange} />
-            <p>The entry form is currently broken, but I'm sure it will be straightforward to get it working, just convert 128 lines of vanilla javascript mangle into React, right?</p>
-            <input type="button" value="new team" id="new" />
-
-            <ul id="users">
-            </ul>
-
-            <input type="text" placeholder="Joe Bloggs" id="user" />
-            <input type="button" value="add" id="add" />
+           
             <ul>
             <li><a href="/react/react.html?model=ComfortZone">Comfort Zone</a></li>
             <li><a href="/react/react.html?model=Tuckman">Tuckman</a></li>
