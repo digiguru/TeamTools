49809c71e47755868db2881df2c30455e8755fa6
digiguru <digiguru@gmail.com>
2026-09-22 16:56:04 +0100

    feat: enable Vercel web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import "./Shared/styles.css";
 import RoomApp from "./LiveSession/RoomApp";
 
 export default function App() {
-  return <RoomApp />;
+  return (
+    <>
+      <RoomApp />
+      <Analytics />
+    </>
+  );
 }
