02ba3d309458899711ee244e426a93885cbcbd57
digiguru <digiguru@gmail.com>
2026-09-22 08:20:15 +0100

    refactor: keep BrowserRepo as live-session compatibility alias

--- src/Shared/BrowserRepo.ts
+++ src/Shared/BrowserRepo.ts
@@ -1,21 +1,2 @@
-import {IAllRepostiory} from "./IUsers";
-
-export class BrowserRepo<T> implements IAllRepostiory<T> {
-    key: string;
-    br: Window;
-    constructor (key: string, window: Window) {
-        this.br = window;
-        this.key = key;
-    }
-    get(): PromiseLike<T> {
-        const text: string = this.br.localStorage.getItem(this.key);
-        const json: T = JSON.parse(text);
-        return Promise.resolve(json);
-    }
-    save(thing: T): PromiseLike<T> {
-        const text = JSON.stringify(thing);
-        this.br.localStorage.setItem(this.key, text);
-        return Promise.resolve(thing);
-    }
-
-}
+export { BrowserRepository as BrowserRepo } from "../LiveSession/BrowserRepository";
+export type { Repository as IAllRepostiory } from "../LiveSession/BrowserRepository";
